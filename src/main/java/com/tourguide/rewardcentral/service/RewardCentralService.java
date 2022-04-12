d694cf0f314f00852a6f591a37ad392f2e92fd33
Guillaume Aubert <8095025-Kormul@users.noreply.gitlab.com>
2022-04-12 15:24:20 +0200

    add executorservice

--- src/main/java/com/tourguide/rewardcentral/service/RewardCentralService.java
+++ src/main/java/com/tourguide/rewardcentral/service/RewardCentralService.java
@@ -1,6 +1,10 @@
 package com.tourguide.rewardcentral.service;
 
 import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -15,10 +19,23 @@ public class RewardCentralService {
 
 	private RewardCentral rewardCentral = new RewardCentral();
 	
+	private ExecutorService executorService = Executors.newFixedThreadPool(32000);
+	
     public int getRewardPoints(UUID attractionId, UUID userId) {
 
     	logger.debug("Get Reward Points");
-        return rewardCentral.getAttractionRewardPoints(attractionId, userId);
+    	
+		
+		try {
+			return CompletableFuture.supplyAsync(()-> {	
+		        return rewardCentral.getAttractionRewardPoints(attractionId, userId);
+			}, executorService ).get();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			e.printStackTrace();
+		}
+		return 0;
     }
 	
 }
